9b6899f10390ba4c89e8e0f45c7744e1b07f3425
Tommie <tommie.rundberg@gmail.com>
2022-02-10 21:07:05 +0100

    updating

---
+++ src/views/Loggedin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loggedin">
+    <h1>Logged in!!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+.loggedin {
+  position: fixed;
+  top: 45px !important;
+  left:0;
+  padding:15px;
+  width:calc(100% - 30px);
+  text-align: center;
+}
+</style>
