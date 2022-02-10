908fc60acf3b6ffdf5bdbcc5a58be58682ee8485
Tommie <tommie.rundberg@gmail.com>
2022-02-10 21:51:19 +0100

    updating

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import router from './router'
 import VueSession from 'vue-session'
 
-Vue.use(VueSession)
+//Vue.use(VueSession)
 
 createApp(App).use(router).mount('#app')
