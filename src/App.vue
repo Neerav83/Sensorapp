ed2f50acdfb07b820d1d1ac5ab4887bea2ca129d
Tommie <tommie.rundberg@gmail.com>
2022-02-10 18:11:39 +0100

    updating

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+   
   </div>
   <router-view />
 </template>
@@ -27,4 +26,8 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+/* <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+*/
 </style>
