ba35e5c7c6c453af6f8199641875514c0ba6be7e
Tommie <tommie.rundberg@gmail.com>
2022-02-10 21:58:39 +0100

    updating

--- src/components/login.vue
+++ src/components/login.vue
@@ -13,7 +13,6 @@
 
 <script>
 import axios from 'axios'
-import VueSession from 'vue-session'
 
 
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueSession from "vue-session";
+App.use(VueSession);
 //Vue.use(VueSession)
 
 createApp(App).use(router).mount('#app')
