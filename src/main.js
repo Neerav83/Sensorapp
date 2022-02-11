616e7da271f46d672211004fb8dfe880e18edc4d
Tommie <tommie.rundberg@gmail.com>
2022-02-11 18:22:07 +0100

    update

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import Vue3Storage from "vue3-storage"
 
 
 createApp(App).use(
-    Vue3Storage,{
-        namespace:"pro_", storage:StorageType.Local
-    },
-    router
+    Vue3Storage, { namespace: "pro_", storage: StorageType.Local },
+        router
     ).mount('#app')
